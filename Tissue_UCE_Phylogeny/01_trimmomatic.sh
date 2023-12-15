75ecf2e632588f6ba846d96ec34f5881d05e30ee
Jack Hruska <jph239@cornell.edu>
2023-12-15 17:16:26 -0600

    Create 01_trimmomatic.sh

---
+++ Tissue_UCE_Phylogeny/01_trimmomatic.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#SBATCH --chdir=./
+#SBATCH --job-nam=ardeidae_trimmomatic
+#SBATCH --partition quanah
+#SBATCH --nodes=1 --ntasks=8
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=10G
+
+conda activate phyluce-1.7.1
+
+illumiprocessor \
+	--input /lustre/scratch/johruska/ardeidae/raw \
+	--output /lustre/scratch/johruska/ardeidae/clean-fastq \
+	--config ./heron_illumiprocessor_all.conf \
+	--cores 8 \
+	--r1-pattern _R1_001 \
+	--r2-pattern _R1_002 \
+	--trimmomatic /lustre/work/johruska/miniconda2/bin/trimmomatic
